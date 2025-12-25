ad7f9e48b91f0b6d4d91039c2dc29666f30e1ab1
Drew <77373359+AjaxSelectButtonGames@users.noreply.github.com>
2025-12-25 14:48:32 -0500

    Add configuration settings for Bluesky bot

---
+++ config.js
@@ -0,0 +1,10 @@
+import "dotenv/config":
+
+export const CONFIG = {
+  username: process.env.BLUESKY_USERNAME,
+  password: process.env.BLUESKY_PASSWORD,
+  api: process.env.ANALYSIS_API,
+  botName: process.env.BOT_NAME,
+  pollInterval: 60_000,
+  cooldownHours: 6
+};
